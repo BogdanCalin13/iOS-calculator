b8ccc3e0a7a5499d7845d5f62d9da7c396741a4b
Bogdan <bogdancalin131196@gmail.com>
2022-12-15 00:31:19 +0200

    Functionality changes

--- index.js
+++ index.js
@@ -1,71 +1,148 @@
 (function () {
-    let state = {
-        number: undefined,
-        operator: undefined,
-        numberSaved: undefined
-    };
+    // let state = {
+    //     number: undefined,
+    //     operator: undefined,
+    //     numberSaved: undefined
+    // };
+    const state = (function () {
+        let number = undefined;
+        let operator = undefined;
+        let numberSaved = undefined;
+        return {
+            restart: function () {
+                number = undefined;
+                operator = undefined;
+                numberSaved = undefined;
+            },
+            // state.getNumber() - este acum echivalentul lui state.number
+            //                     dar nu mai exista acum state.number
+            getNumber: function () {
+                return number;
+            },
+            // state.getOperator() - este acum echivalentul lui state.operator
+            //                       dar nu mai exista acum state.operator
+            getOperator: function () {
+                return operator;
+            },
+            // state.getNumberSaved() - este acum echivalentul lui state.numberSaved
+            //                          dar nu mai exista acum state.numberSaved
+            getNumberSaved: function () {
+                return numberSaved;
+            },
+            // state.setNumber(178 | 63 | 1 | 9 | ...) - este acum echivalentul lui state.number = 178 | 63 | 1 | 9 | ...
+            //                                           dar nu mai putem atribui direct state.number = 178 | 63 | 1 | 9 | ...
+            setNumber: function (newNumber) {
+                number = newNumber;
+            },
+            // state.setOperator("+" | "-" | "*" | "=" | "/") - este acum echivalentul lui state.operator = "+" | "-" | "*" | "=" | "/"
+            //                                                  dar nu mai putem atribui direct state.operator = "+" | "-" | "*" | "=" | "/"
+            setOperator: function (newOperator) {
+                operator = newOperator;
+            },
+            // state.setNumberSaved(true|false) - este acum echivalentul lui state.numberSaved = true|false
+            //                          dar nu mai putem atribui direct state.numberSaved = true|false
+            setNumberSaved: function (newNumberSaved) {
+                numberSaved = newNumberSaved;
+            }
+        }
+    })();
 
 
-    // alternativ cel mai probabil ati vazut cu document.getElementById("#calculator-output");
-    // document.querySelectorAll(".input") <=> document.getElementsByClassName("input");
     const calculatorOutput = document.querySelector(".calculator-output");
-    // console.log(calculatorOutput);
 
-    // luam toate butoanele care schimba numerele
     const inputs = document.querySelectorAll(".input");
-    // console.log(inputs);
+    const operators = document.querySelectorAll("button[data-operator]");
+    const signChange = document.querySelector(".sign-change");
+    const reset = document.querySelector(".reset");
 
-    // pe toate butoanele care schimba numerele adaugam
-    // functionalitate de adaugare de numere in calculator
     inputs.forEach((item) => {
         item.addEventListener("click", function () {
-            // console.log("you have clicked: ", item.getAttribute("data-number"));
-            // // acest .dataset, functioneaza doar pentru atribute care incep cu data-
-            // console.log("you have clicked: ", item.dataset.number);
-            // console.log("you have clicked: ", item.dataset);
-            if ((calculatorOutput.innerHTML === '0' && item.dataset.number !== ".") || state.numberSaved) {
+            if ((calculatorOutput.innerHTML === '0' && item.dataset.number !== ".") || state.getNumberSaved()) {
                 calculatorOutput.innerHTML = item.dataset.number;
-                state.numberSaved = false;
+                state.setNumberSaved(false);
             } else {
-                calculatorOutput.innerHTML += item.dataset.number;
+                let fontSize = 64;
+                for (let i = 10; i <= 17; i++) {
+                    if (calculatorOutput.innerHTML.length >= i) {
+                        fontSize = fontSize * 0.91;
+                    }
+                }
+
+                calculatorOutput.style.fontSize = fontSize + "px";
+                if (calculatorOutput.innerHTML.length < 17) {
+                    calculatorOutput.innerHTML += item.dataset.number;
+                }
             }
+            reset.innerHTML = "C";
         });
     });
 
-    // luam toate butoanele care sunt operatori
-    const operators = document.querySelectorAll("button[data-operator]");
-    // pe toate butoanele care sunt operatori adaugam
-    // functionalitate de memorare de operatie viitoare
     operators.forEach((item) => {
         item.addEventListener("click", function () {
-            if (state.operator && state.number) {
-                switch (state.operator) {
-                    case "%":
-                        calculatorOutput.innerHTML = parseFloat(state.number) % parseFloat(calculatorOutput.innerHTML);
-                        break;
+            operators.forEach(item => {
+                item.classList.remove("active");
+            });
+            if (item.dataset.operator !== "=") {
+                item.classList.add("active");
+            }
+            if (item.dataset.operator === "%") {
+                calculatorOutput.innerHTML = parseFloat(calculatorOutput.innerHTML) / 100;
+            } else if (state.getOperator() && state.getNumber()) {
+                switch (state.getOperator()) {
                     case "/":
-                        calculatorOutput.innerHTML = parseFloat(state.number) / parseFloat(calculatorOutput.innerHTML);
+                        calculatorOutput.innerHTML = parseFloat(state.getNumber()) / parseFloat(calculatorOutput.innerHTML);
                         break;
                     case "*":
-                        calculatorOutput.innerHTML = parseFloat(state.number) * parseFloat(calculatorOutput.innerHTML);
+                        calculatorOutput.innerHTML = parseFloat(state.getNumber()) * parseFloat(calculatorOutput.innerHTML);
                         break;
                     case "-":
-                        calculatorOutput.innerHTML = parseFloat(state.number) - parseFloat(calculatorOutput.innerHTML);
+                        calculatorOutput.innerHTML = parseFloat(state.getNumber()) - parseFloat(calculatorOutput.innerHTML);
                         break;
                     case "+":
-                        calculatorOutput.innerHTML = parseFloat(state.number) + parseFloat(calculatorOutput.innerHTML);
+                        calculatorOutput.innerHTML = parseFloat(state.getNumber()) + parseFloat(calculatorOutput.innerHTML);
                         break;
                     default:
                         break;
                 }
-
             }
-            state.numberSaved = true;
-            state.number = parseFloat(calculatorOutput.innerHTML);
-            state.operator = item.dataset.operator;
-            // console.log(state);
+            state.setNumberSaved(true);
+            state.setNumber(parseFloat(calculatorOutput.innerHTML));
+            state.setOperator(item.dataset.operator);
+            console.log(state);
         })
-    })
+    });
 
+    signChange.addEventListener("click", function () {
+        if (calculatorOutput.innerHTML !== "0") {
+            calculatorOutput.innerHTML = -parseFloat(calculatorOutput.innerHTML);
+        }
+    });
+
+    reset.addEventListener("click", function () {
+        operators.forEach(item => {
+            item.classList.remove("active");
+        });
+        switch (reset.innerHTML) {
+            case "AC":
+                // state = {
+                //     number: undefined,
+                //     operator: undefined,
+                //     numberSaved: undefined
+                // }
+                // state.setNumber(undefined);
+                // state.setOperator(undefined);
+                // state.setNumberSaved(undefined);
+                state.restart();
+                calculatorOutput.innerHTML = "0";
+                reset.innerHTML = "AC";
+                break;
+            case "C":
+                calculatorOutput.innerHTML = "0";
+                reset.innerHTML = "AC";
+                break;
+            default:
+                break;
+        }
+    })
 
 }());
